e8ff2ff2c049de1af72f82a4885e5b83725b551b
Xinchao Song <git@xinchaosong.com>
2022-09-01 08:39:02 -0400

    First commit

---
+++ src/HelloMain.java
@@ -0,0 +1,29 @@
+// This is my first code
+import java.util.*;
+
+/**
+ * This is a HelloMain class for demo.
+ * @author Xinchao
+ */
+public class HelloMain {
+    /**
+     * This is the maximum number.
+     */
+    public static final int MAX_NUM = 10;
+
+    /***
+     * This is our main method.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+
+        /*
+        this is the first commend
+        this is the second commend
+         */
+        System.out.println("Hello World!"); // This is hello world
+
+        int firstNum = 3;
+        System.out.println("The number " + firstNum + " is less than " + MAX_NUM);
+    }
+}
